879726746727715735da348038e711e0fea3e2ae
Linhholi <103567688+Linhholi@users.noreply.github.com>
2023-06-24 13:32:21 +1000

    Update customers.jsx

--- frontend/src/pages/customers/customers.jsx
+++ frontend/src/pages/customers/customers.jsx
@@ -70,7 +70,7 @@ const Customers = () => {
   }
 
   const fetchData = async () => {
-    const response = await axios.get('http://localhost:3001/api/get-customers');
+    const response = await axios.get('https://linh-react.onrender.com/api/get-customers');
     setData(response.data)
   };
 
